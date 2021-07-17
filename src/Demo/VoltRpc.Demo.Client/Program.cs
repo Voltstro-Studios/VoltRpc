c9c9d489439ae44a9dcf9a0ffb77b2d3fdf88640
Voltstro <me@voltstro.dev>
2021-07-17 21:47:47 +1000

    Update demo

--- src/Demo/VoltRpc.Demo.Client/Program.cs
+++ src/Demo/VoltRpc.Demo.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Net;
 using VoltRpc.Communication.TCP;
 
@@ -11,13 +12,40 @@ namespace VoltRpc.Demo.Client
             TCPClient client = new TCPClient(new IPEndPoint(IPAddress.Loopback, 7678));
             client.Connect();
             
-            client.InvokeMethod("VoltRpc.Demo.Shared.ITest.BasicTest");
-            client.InvokeMethod("VoltRpc.Demo.Shared.ITest.ParmTest", "Hello World!");
+            //Basic test
+            Console.WriteLine("Basic test #1");
+            BasicTest(client);
+            
+            Console.WriteLine("Basic test #2");
+            BasicTest(client);
+            
+            //Parm test
+            Console.WriteLine("Parm test #1");
+            ParmTest(client);
+            
+            Console.WriteLine("Parm test #2");
+            ParmTest(client);
 
             Console.WriteLine("Press any key to quit...");
             Console.ReadKey();
 
             client.Dispose();
+        }
+
+        private static void BasicTest(Communication.Client client)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            client.InvokeMethod("VoltRpc.Demo.Shared.ITest.BasicTest");
+            sw.Stop();
+            Console.WriteLine($"Basic test took: {sw.ElapsedMilliseconds}ms");
+        }
+        
+        private static void ParmTest(Communication.Client client)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            client.InvokeMethod("VoltRpc.Demo.Shared.ITest.ParmTest", "Hello World!");
+            sw.Stop();
+            Console.WriteLine($"Basic test took: {sw.ElapsedMilliseconds}ms");
         }
     }
 }
