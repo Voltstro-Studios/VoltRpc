bc5dc6031c1e3a3d0006d13dddaa07dff48b9750
Voltstro <me@voltstro.dev>
2021-07-25 02:09:27 +1000

    Implement interface

--- src/VoltRpc.Proxy.Generator/ProxyGenerator.cs
+++ src/VoltRpc.Proxy.Generator/ProxyGenerator.cs
@@ -1,5 +1,7 @@
 ﻿using System.Linq;
+using System.Text;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace VoltRpc.Proxy.Generator
@@ -26,7 +28,7 @@ namespace VoltRpc.Proxy
 
 namespace VoltRpc.Proxy.Generated
 {{
-    public class {0}
+    public class {0} : {1}
     {{
         private readonly Client client;
 
@@ -34,6 +36,8 @@ namespace VoltRpc.Proxy.Generated
         {{
             this.client = client;
         }}
+
+        {2}
     }}
 }}";
         
@@ -56,20 +60,75 @@ namespace VoltRpc.Proxy.Generated
                     if (model == null || model.SyntaxTree != interfaceDeclaration.SyntaxTree)
                         model = context.Compilation.GetSemanticModel(interfaceDeclaration.SyntaxTree);
 
-                    ISymbol type = model.GetDeclaredSymbol(interfaceDeclaration);
+                    ISymbol type = ModelExtensions.GetDeclaredSymbol(model, interfaceDeclaration);
                     AttributeData attributeData = type.GetAttributes().FirstOrDefault(x => 
                         SymbolEqualityComparer.Default.Equals(x.AttributeClass, attributeSymbol));
                     
                     if (attributeData != null)
-                        GenerateInterfaceProxy(context, interfaceDeclaration);
+                        GenerateInterfaceProxy(context, model, interfaceDeclaration);
                 }
             }
         }
 
-        private void GenerateInterfaceProxy(GeneratorExecutionContext context, InterfaceDeclarationSyntax interfaceDeclaration)
+        private void GenerateInterfaceProxy(GeneratorExecutionContext context, SemanticModel model, InterfaceDeclarationSyntax interfaceDeclaration)
         {
-            string interfaceProxyName = $"{interfaceDeclaration.Identifier.ValueText}Proxy";
-            context.AddSource(interfaceProxyName, string.Format(BaseText, interfaceProxyName));
+            INamedTypeSymbol interfaceSymbol = model.GetDeclaredSymbol(interfaceDeclaration);
+            string interfaceName = $"{interfaceDeclaration.Identifier.ValueText}";
+            string interfaceProxyName = $"{interfaceName}_GeneratedProxy";
+            string interfaceNamespace = interfaceSymbol.ContainingNamespace.ToString();
+                
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (MemberDeclarationSyntax memberDeclarationSyntax in interfaceDeclaration.Members)
+            {
+                if(memberDeclarationSyntax.Modifiers.All(modifer => modifer.Kind() != SyntaxKind.PublicKeyword))
+                    continue;
+                
+                MethodDeclarationSyntax method = (MethodDeclarationSyntax) memberDeclarationSyntax;
+                stringBuilder.Append(CreateMethod(model, method));
+            }
+            
+            context.AddSource(interfaceProxyName, string.Format(BaseText, interfaceProxyName, $"{interfaceNamespace}.{interfaceName}", stringBuilder));
+        }
+
+        private string CreateMethod(SemanticModel model, MethodDeclarationSyntax method)
+        {
+            IMethodSymbol methodSymbol = model.GetDeclaredSymbol(method);
+            
+            StringBuilder methodSb = new StringBuilder();
+            methodSb.Append($"\npublic {method.ReturnType.ToFullString()}{method.Identifier.ValueText}(");
+
+            //Create parms
+            int parametersCount = methodSymbol.Parameters.Length;
+            for (int i = 0; i < parametersCount; i++)
+            {
+                IParameterSymbol symbol = methodSymbol.Parameters[i];
+                switch (symbol.RefKind)
+                {
+                    case RefKind.Ref:
+                        methodSb.Append("ref ");
+                        break;
+                    case RefKind.Out:
+                        methodSb.Append("out ");
+                        break;
+                }
+                
+                methodSb.Append($"{symbol.Type} {symbol.Name}");
+                
+                if (i + 1 != parametersCount)
+                    methodSb.Append(", ");
+            }
+
+            methodSb.Append(")\n{\n");
+
+            //TODO: Implement 
+            if (!methodSymbol.ReturnsVoid)
+            {
+                methodSb.Append("return null;\n");
+            }
+
+            methodSb.Append("}\n");
+            
+            return methodSb.ToString();
         }
     }
 }
