abec462ef190fbc240317d32d7e2dbbf56d25de1
Voltstro <me@voltstro.dev>
2021-07-31 17:07:37 +1000

    Added pipes benchmark

---
+++ src/VoltRpc.Benchmarks/PipesBenchmark.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using VoltRpc.Benchmarks.Core;
+using VoltRpc.Communication.Pipes;
+
+namespace VoltRpc.Benchmarks
+{
+    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    [SimpleJob(RuntimeMoniker.Net50)]
+    public class PipesBenchmark : VoltRpcBenchmark
+    {
+        private const string PipesName = "BenchmarkPipe";
+
+        public PipesBenchmark()
+            : base(new PipesClient(PipesName), new PipesHost(PipesName, 128))
+        {
+        }
+    }
+}
