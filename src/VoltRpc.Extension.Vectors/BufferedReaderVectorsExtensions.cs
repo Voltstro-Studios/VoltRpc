8dfa8b806eaf299fa2e0385fa1fdec5c367d0885
Voltstro <me@voltstro.dev>
2022-05-20 20:45:02 +1000

    XML

--- src/VoltRpc.Extension.Vectors/BufferedReaderVectorsExtensions.cs
+++ src/VoltRpc.Extension.Vectors/BufferedReaderVectorsExtensions.cs
@@ -8,36 +8,57 @@ namespace VoltRpc.Extension.Vectors;
 /// </summary>
 public static class BufferedReaderVectorsExtensions
 {
+    /// <summary>
+    ///     Reads a <see cref="Matrix3x2"/>
+    /// </summary>
     public static Matrix3x2 ReadMatrix3X2(this BufferedReader reader)
     {
         return reader.ReadBlittable<Matrix3x2>();
     }
 
+    /// <summary>
+    ///     Reads a <see cref="Matrix4x4"/>
+    /// </summary>
     public static Matrix4x4 ReadMatrix4X4(this BufferedReader reader)
     {
         return reader.ReadBlittable<Matrix4x4>();
     }
 
+    /// <summary>
+    ///     Reads a <see cref="Plane"/>
+    /// </summary>
     public static Plane ReadPlane(this BufferedReader reader)
     {
         return reader.ReadBlittable<Plane>();
     }
 
+    /// <summary>
+    ///     Reads a <see cref="Quaternion"/>
+    /// </summary>
     public static Quaternion ReadQuaternion(this BufferedReader reader)
     {
         return reader.ReadBlittable<Quaternion>();
     }
 
+    /// <summary>
+    ///     Reads a <see cref="Vector2"/>
+    /// </summary>
     public static Vector2 ReadVector2(this BufferedReader reader)
     {
         return reader.ReadBlittable<Vector2>();
     }
 
+    /// <summary>
+    ///     Reads a <see cref="Vector3"/>
+    /// </summary>
     public static Vector3 ReadVector3(this BufferedReader reader)
     {
         return reader.ReadBlittable<Vector3>();
     }
 
+    /// <summary>
+    ///     Reads a <see cref="Vector4"/>
+    /// </summary>
     public static Vector4 ReadVector4(this BufferedReader reader)
     {
         return reader.ReadBlittable<Vector4>();
